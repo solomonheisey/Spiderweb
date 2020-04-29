e33532f2b741641daf964f4315d94777788e16c8
Solomon Heisey <49796963+solomonheisey@users.noreply.github.com>
2020-04-29 09:55:22 -0400

    changing colors works and so does brightness adjustment

--- src/application/Spiderweb.py
+++ src/application/Spiderweb.py
@@ -1,6 +1,8 @@
 from copy import copy
-from lifxlan import GREEN, LifxLAN, ORANGE, RED, WHITE, YELLOW
+from lifxlan import GREEN, LifxLAN, ORANGE, RED, WHITE
 from scapy.all import *
+from tkinter import *
+from tkinter import colorchooser
 from time import sleep, time
 
 import cdb
@@ -12,7 +14,9 @@ import signal
 import sys
 import threading
 import time
+import Tkinter
 import tkinter as tk
+
 
 # List of unique MAC address and list of MAC addresses match to vendor name
 clients = []
@@ -146,6 +150,142 @@ def update_database():
             db.add(mac, vendor)
         db.finish()
 
+
+
+def rgb_to_hsv(r, g, b):
+    r = float(r)
+    g = float(g)
+    b = float(b)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h, s, v = high, high, high
+
+    d = high - low
+    s = 0 if high == 0 else d / high
+
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d + (6 if g < b else 0),
+            g: (b - r) / d + 2,
+            b: (r - g) / d + 4,
+        }[high]
+        h /= 6
+
+    return h, s, v
+
+def high():
+    global high_traffic
+    clr = colorchooser.askcolor(parent=root)
+    b1.configure(bg=clr[1])
+    temp =  str(clr[1]).lstrip('#')
+    RGB = tuple(int(temp[i:i+2], 16) for i in (0, 2, 4))
+    hsv = rgb_to_hsv(RGB[0] / 255.0, RGB[1] / 255.0, RGB[2] / 255.0)
+
+    bulbHSBK = [hsv[0] * 65535.0,hsv[1] * 65535.0,hsv[2] * 65535.0,3500]
+    gCycleHue = bulbHSBK[0]
+    gCycleSaturation = bulbHSBK[1]
+    gCycleBrightness = bulbHSBK[2]
+
+    high_traffic[0] = bulbHSBK[0]
+    high_traffic[1] = bulbHSBK[1]
+    high_traffic[2] = bulbHSBK[2]
+    high_traffic[3] = bulbHSBK[3]
+
+    lifxlan.set_color_all_lights(bulbHSBK, duration=0, rapid=False)
+
+def med():
+    global medium_traffic
+    clr = colorchooser.askcolor(parent=root)
+    b2.configure(bg=clr[1])
+    temp =  str(clr[1]).lstrip('#')
+    RGB = tuple(int(temp[i:i+2], 16) for i in (0, 2, 4))
+    hsv = rgb_to_hsv(RGB[0] / 255.0, RGB[1] / 255.0, RGB[2] / 255.0)
+
+    bulbHSBK = [hsv[0] * 65535.0,hsv[1] * 65535.0,hsv[2] * 65535.0,3500]
+    gCycleHue = bulbHSBK[0]
+    gCycleSaturation = bulbHSBK[1]
+    gCycleBrightness = bulbHSBK[2]
+    
+    medium_traffic[0] = bulbHSBK[0]
+    medium_traffic[1] = bulbHSBK[1]
+    medium_traffic[2] = bulbHSBK[2]
+    medium_traffic[3] = bulbHSBK[3]
+
+    lifxlan.set_color_all_lights(bulbHSBK, duration=0, rapid=False)
+
+def low():
+    global low_traffic
+    clr = colorchooser.askcolor(parent=root)
+    b3.configure(bg=clr[1])
+    temp =  str(clr[1]).lstrip('#')
+    RGB = tuple(int(temp[i:i+2], 16) for i in (0, 2, 4))
+    hsv = rgb_to_hsv(RGB[0] / 255.0, RGB[1] / 255.0, RGB[2] / 255.0)
+
+    bulbHSBK = [hsv[0] * 65535.0,hsv[1] * 65535.0,hsv[2] * 65535.0,3500]
+    gCycleHue = bulbHSBK[0]
+    gCycleSaturation = bulbHSBK[1]
+    gCycleBrightness = bulbHSBK[2]
+    
+    low_traffic[0] = bulbHSBK[0]
+    low_traffic[1] = bulbHSBK[1]
+    low_traffic[2] = bulbHSBK[2]
+    low_traffic[3] = bulbHSBK[3]
+
+    lifxlan.set_color_all_lights(bulbHSBK, duration=0, rapid=False)
+
+def confirm():
+    breathe(WHITE)
+    root.destroy()
+
+def scale(v):
+    global variable 
+    variable = v
+
+def set_brightness():
+
+    val = scale.get()
+
+    WHITE[2] = val
+
+    low_traffic[2] = val
+    medium_traffic[2] = val
+    high_traffic[2] = val
+
+
+
+
+
+    brightnessWindow.destroy()
+    lifxlan.set_color_all_lights(WHITE, duration=0, rapid=False)
+
+def brightness():
+    global brightnessWindow
+    global scale
+
+    brightnessWindow = tk.Toplevel(root)
+    brightnessWindow.geometry('250x150')
+
+    colors = lifxlan.get_color_all_lights()
+    for bulb in colors:
+        color = bulb.get_color()
+    scale = tk.Scale(brightnessWindow, orient='vertical', from_=65535, to=0)
+    scale.set(int(color[2]))
+    scale.pack(anchor=CENTER)
+
+    button = tk.Button(brightnessWindow, text="Set Brightness", command=set_brightness)   
+    button.pack(anchor=CENTER)
+
+def reset():
+    high_traffic = RED
+    medium_traffic = ORANGE
+    low_traffic = GREEN
+
+    b1.configure(bg='red')
+    b2.configure(bg='orange')
+    b3.configure(bg='green')    
+
 if __name__ == "__main__":
     global lifxlan
     global high_traffic
@@ -225,6 +365,31 @@ if __name__ == "__main__":
             print('For the next 1 minute, this device will be collecting data in order to create a baseline to classify the '
                   'data being received')
             print(' ')
+            os.system('clear')
+            print('Please configure your settings in the pop-up window. Press confirm to confirm your settings')
+
+
+            root = tk.Tk()
+            root.geometry("250x500")
+            root.title("Spiderweb")
+            b1 = tk.Button(root, text='Set High Traffic Color', bg='red', command=high, height=2, width=20)
+            b1.pack(side=TOP,pady=(10,0))
+            b2 = tk.Button(root, text='Set Medium Traffic Color', bg='orange', command=med, height=2, width=20)
+            b2.pack(side=TOP)
+            b3 = Button(root, text='Set Low Traffic Color', bg='green', command=low, height=2, width=20)
+            b3.pack(side=TOP)
+
+            b5 = tk.Button(root, text='Brightness', command=brightness, height=2, width=20)
+            b5.pack(side=TOP, pady=(40,0))
+            b6 = tk.Button(root, text='Pulse Delay', height=2, width=20)
+            b6.pack(side=TOP)
+
+            b7 = tk.Button(root, text='Reset', command=reset, height=2, width=20)
+            b7.pack(side=BOTTOM)
+
+            b4 = tk.Button(root, text='Confirm', command=confirm, height=2, width=20)
+            b4.pack(side=BOTTOM,pady=(0,10))
+            root.mainloop()
 
     	# gathers data from given MAC address for 1 minute and calculates average by also removing outliers
             baseline(mac_address) 
